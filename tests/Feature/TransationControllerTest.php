1a34ea6fd7efc6bfb80045abc12174657ffa39e3
lorenzomene <lorenzospmene@gmail.com>
2024-07-17 15:56:43 -0300

    feat: first test for tdd approach

---
+++ tests/Feature/TransationControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Wallet;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TransationControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_post_payment(): void
+    {
+        //Prepare
+        $payer = Wallet::factory()->create();
+        $payee = Wallet::factory()->create();
+        $value = 100;
+
+        //Act
+        $response = $this->postJson(route('transactions.create'), [
+            'payer_id' => $payer->getKey(),
+            'payee_id' => $payee->getKey(),
+            'value' => $value,
+        ]);
+
+        //Assert
+        $response->assertNoContent();
+    }
+}
