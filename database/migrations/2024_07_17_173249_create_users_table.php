59dc3331269ef7f9565dffebaac243e66155cd8a
lorenzomene <lorenzospmene@gmail.com>
2024-07-17 14:58:40 -0300

    feat: small refactor on db schema + migrations

---
+++ database/migrations/2024_07_17_173249_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer('document_type');
+            $table->string('document_number');
+            $table->integer('user_type')->index();
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->timestamps();
+
+            $table->unique(['document_type', 'document_number']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2024_07_17_173322_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->integer('value');
+            $table->foreignUuid('payer_id')->constrained('wallet')->onDelete('cascade');
+            $table->foreignUuid('payee_id')->constrained('wallet')->onDelete('cascade');
+            $table->integer('status')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
