6092ee0d6ce3cdf85f18be097084e6429bced0c1
lorenzomene <lorenzospmene@gmail.com>
2024-07-18 00:35:12 -0300

    feat: add relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Enums\DocumentTypeEnum;
+use App\Enums\UserTypeEnum;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Model
 {
@@ -22,5 +25,16 @@ class User extends Model
         'document_number',
         'user_type',
     ];
+
+    protected static function newFactory()
+    {
+        return \Database\Factories\User\UserFactory::new();
+    } 
+
+
+    public function wallet(): HasOne
+    {
+        return $this->hasOne(Wallet::class);
+    }
 
 }
--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Wallet extends Model
 {
@@ -28,4 +29,9 @@ class Wallet extends Model
     {
         return \Database\Factories\Wallet\WalletFactory::new();
     } 
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
