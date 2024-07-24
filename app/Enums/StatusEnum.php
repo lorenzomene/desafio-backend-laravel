7a26d1df6599a8c0c46fa1f0cf997acc906cf781
lorenzomene <lorenzospmene@gmail.com>
2024-07-24 16:56:32 -0300

    fix: enum refactor

--- app/Enums/StatusEnum.php
+++ app/Enums/StatusEnum.php
@@ -2,29 +2,11 @@
 
 namespace App\Enums;
 
-enum StatusEnum: int {
+enum StatusEnum: string {
     
-    case Pending = 1;
-    case Error = 2;
-    case Success = 3;
+    case Created = 'created';
+    case Withdraw = 'withdraw';
+    case Debited = 'debited';
+    case Completed = 'completed';
     
-    public static function toString(int $value): ?string
-    {
-        return match($value) {
-            self::Pending->value => 'Pending',
-            self::Error->value => 'Error',
-            self::Success->value => 'Success',
-            default => 'Pending',
-        };
-    }
-
-    public static function toInt(string $name): ?int
-    {
-        return match($name) {
-            'Pending' => self::Pending->value,
-            'Error' => self::Error->value,
-            'Success' => self::Success->value,
-            default => self::Pending->value,
-        };
-    }
 }
