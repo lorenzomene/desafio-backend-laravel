f4cf4a460d33f0def7d67553b1604644fffa57b9
lorenzomene <lorenzospmene@gmail.com>
2024-07-24 13:34:53 -0300

    feat: update test to contain full transaction

--- tests/Feature/TransationControllerTest.php
+++ tests/Feature/TransationControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enums\StatusEnum;
 use App\Models\Wallet;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -14,9 +15,13 @@ class TransationControllerTest extends TestCase
     public function test_post_payment(): void
     {
         // Prepare
-        $payer = Wallet::factory()->create();
-        $payee = Wallet::factory()->create();
-        $value = 100;
+        $payer = Wallet::factory()->create([
+            'balance' => 10_00
+        ]);
+        $payee = Wallet::factory()->create([
+            'balance' => 0
+        ]);
+        $value = 5_00;
 
         // Act
         $response = $this->postJson(route('transactions.create'), [
@@ -27,5 +32,23 @@ class TransationControllerTest extends TestCase
 
         // Assert
         $response->assertNoContent();
+
+
+        $this->assertDatabaseHas('transactions', [
+            'payer_id' => $payer->getKey(), 
+            'payee_id' => $payee->getKey(), 
+            'amount' => 1000, 
+            'status' => StatusEnum::Completed,
+        ]);
+
+        $this->assertDatabaseHas('wallets', [
+            'id' => $payer->getKey(), 
+            'balance' => 5_00
+        ]);
+        
+        $this->assertDatabaseHas('wallets', [
+            'id' => $payee->getKey(), 
+            'balance' => 5_00
+        ]);
     } 
 }
